0d2fa7543a0dc331bbf2487a9d28de7c463566fd
saeh00 <saufi2810@gmail.com>
2021-11-20 00:21:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/wof/GameFragment.kt
@@ -0,0 +1,72 @@
+package com.example.wof
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.view.isVisible
+import androidx.fragment.app.FragmentTransaction
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GameFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GameFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_game, container, false)
+
+        val spinButton = view.findViewById<Button>(R.id.wordPicker)
+        val guessField = view.findViewById<EditText>(R.id.guessBox)
+        spinButton.setOnClickListener{
+            guessField.visibility = View.VISIBLE
+            spinButton.visibility = View.GONE
+
+        }
+
+        return view
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment GameFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            GameFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/wof/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.wof
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils.replace
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.FragmentManager
+
+class MainActivity : AppCompatActivity() {
+
+    var startButton: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startButton = findViewById(R.id.startButton)
+
+        //add the fragment here
+        val startFragment = StartFragment()
+        val fm: FragmentManager = supportFragmentManager
+        fm.beginTransaction().add(R.id.container,startFragment).commit()
+
+
+        //supportFragmentManager.beginTransaction().add(R.id.container, GameFragment()).commit()
+
+    }
+
+}
