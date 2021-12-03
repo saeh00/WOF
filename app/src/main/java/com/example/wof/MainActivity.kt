4bb4decd04c715bf968c79c0f98b645cc58a56d7
saeh00 <saufi2810@gmail.com>
2021-12-03 03:46:51 +0100

    Removed some redundant code, small changes in layout, and added a rules fragment. Also staring implementing categories, so they eventually can work.

--- app/src/main/java/com/example/wof/MainActivity.kt
+++ app/src/main/java/com/example/wof/MainActivity.kt
@@ -2,27 +2,11 @@ package com.example.wof
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils.replace
-import android.view.View
-import android.widget.Button
-import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
 
-    var startButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-/*
-        startButton = findViewById(R.id.startButton)
-
-        //add the fragment here
-        val startFragment = StartFragment()
-        val fm: FragmentManager = supportFragmentManager
-        fm.beginTransaction().add(R.id.container,startFragment).commit()
-
-
- */
     }
-
 }
